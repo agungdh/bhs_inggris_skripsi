9e4cdd476ccde48235028e5b8246e7db459d841c
AgungDH <agungdh@live.com>
2019-07-06 15:57:17 +0700

    no message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
-    /**asfasfasfasf
+    /**
      * A list of the exception types that are not reported.
      *
      * @var array
