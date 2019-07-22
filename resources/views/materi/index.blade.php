bf06271fe472a65a25890adce13a0c6eb6344fd8
AgungDH <agungdh@live.com>
2019-07-22 23:22:07 +0700

    .

--- resources/views/materi/index.blade.php
+++ resources/views/materi/index.blade.php
@@ -10,32 +10,32 @@ Materi
 
 @section('content')
 <div class="row">
-	<div class="col-md-12">
-		<div class="box animated slideInLeft box-primary" style="box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);">
+    <div class="col-md-12">
+        <div class="box animated slideInLeft box-primary" style="box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);">
             <div class="box-header">
               <h3 class="box-title">Data Materi</h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
                 @if(ADHhelper::getUserData()->level == 'a')
-            	<a class="btn btn-success btn-sm" href="{{route('materi.create')}}">
+                <a class="btn btn-success btn-sm" href="{{route('materi.create')}}">
                   <i class="glyphicon glyphicon-plus"></i> Tambah
                 </a><br><br>
                 @endif
               <table class="table table-bordered table-hover datatable" style="width: 100%">
                 <thead>
-	                <tr>
-	                  <th>Unit</th>
+                    <tr>
+                      <th>Unit</th>
                     <th>Materi</th>
                     <th>Deskripsi</th>
                     @if(ADHhelper::getUserData()->level == 's')
                     <th>Nilai</th>
                     @endif
-	                  <th>Proses</th>
-	                </tr>
+                      <th>Proses</th>
+                    </tr>
                 </thead>
                 <tbody>
-                	@foreach($materis as $item)
+                    @foreach($materis as $item)
 
                     @php
                         $nilai = DB::table('ujian')->where([
@@ -44,21 +44,21 @@ Materi
                         ])->first();
                     @endphp
 
-                	<tr>
-                		<td>{{$item->unit}}</td>
+                    <tr>
+                        <td>{{$item->unit}}</td>
                         <td>{{$item->materi}}</td>
                         <td>{{$item->deskripsi}}</td>
                         @if(ADHhelper::getUserData()->level == 's')
                         <td>{{$nilai ? $nilai->nilai : '-'}}</td>
                         @endif
-                		
-                		<td>
+                        
+                        <td>
 
-			                {!! Form::open(['id' => 'formHapus' . $item->id, 'route' => ['materi.destroy', $item->id], 'method' => 'delete']) !!}
+                            {!! Form::open(['id' => 'formHapus' . $item->id, 'route' => ['materi.destroy', $item->id], 'method' => 'delete']) !!}
 
                                 <a target="_blank" class="btn btn-default btn-sm" href="{{route('materi.berkas', $item->id)}}">
-				                  <i class="glyphicon glyphicon-file"></i> Berkas
-				                </a>
+                                  <i class="glyphicon glyphicon-file"></i> Berkas
+                                </a>
 
                                 @if(ADHhelper::getUserData()->level == 'a')
                                 <a class="btn btn-default btn-sm" href="{{route('narasi.index', $item->id)}}">
@@ -73,39 +73,39 @@ Materi
                                   <i class="glyphicon glyphicon-pencil"></i> Edit
                                 </a>
 
-			              		<button type="button" class="btn btn-danger btn-sm" onclick="hapus('{{ $item->id }}')"><i class="glyphicon glyphicon-trash"></i> Hapus</button>
+                                <button type="button" class="btn btn-danger btn-sm" onclick="hapus('{{ $item->id }}')"><i class="glyphicon glyphicon-trash"></i> Hapus</button>
                                 @else
                                 <a class="btn btn-default btn-sm" href="{{route('materi.ujian', $item->id)}}">
                                   <i class="glyphicon glyphicon-question-sign"></i> Ujian
                                 </a>
                                 @endif
 
-			              	{!! Form::close() !!}
-                		</td>
-                	</tr>
-                	@endforeach
+                            {!! Form::close() !!}
+                        </td>
+                    </tr>
+                    @endforeach
                 </tbody>
               </table>
             </div>
             <!-- /.box-body -->
           </div>
-	</div>
+    </div>
 </div>
 @endsection
 
 @section('js')
 <script type="text/javascript">
 function hapus(id) {
-	swal({
-	  title: "Yakin Hapus ???",
-	  text: "Data yang sudah dihapus tidak dapat dikembalikan lagi !!!",
-	  type: "warning",
-	  showCancelButton: true,
-	  confirmButtonColor: "#DD6B55",
-	  confirmButtonText: "Hapus",
-	}, function(){
-	  $("#formHapus" + id).submit();
-	});
+    swal({
+      title: "Yakin Hapus ???",
+      text: "Data yang sudah dihapus tidak dapat dikembalikan lagi !!!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Hapus",
+    }, function(){
+      $("#formHapus" + id).submit();
+    });
 }
 </script>
 @endsection
